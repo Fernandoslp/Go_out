163eddc0dfeef2d661ba37b175f16c4a641f7556
Fernando Sousa Matos <est-fernando.matos@tjsp.jus.br>
2024-09-11 12:39:39 -0300

    Early stage

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados= [
+    {
+        titulo : "Varandeck",
+        descricao : "Endereço:Rua irineu bonard 173 ",
+        link: "https://www.instagram.com/varandeck"
+     },
+     {
+        titulo : "Nakka Bar",
+        descricao : "Endereço:",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+     },
+     {
+        titulo : "itaquera beach bar",
+        descricao : "Endereço:",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+     },
+     {
+        titulo : "Amém lounge Store",
+        descricao : "Endereço:",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+     }
+
+ ];
