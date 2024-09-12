3ea9703d1f6f387a54a2b6bdaa8e63ddadf5e42d
Fernando Sousa Matos <est-fernando.matos@tjsp.jus.br>
2024-09-12 12:53:42 -0300

    dados dos locais

--- dados.js
+++ dados.js
@@ -1,23 +1,33 @@
 let dados= [
     {
         titulo : "Varandeck",
-        descricao : "Endereço:Rua irineu bonard 173 ",
-        link: "https://www.instagram.com/varandeck"
+        descricao : "Endereço:Rua Irineu Bonardi, 173 - Vila Pedroso, São Paulo - SP, 08011-180",
+        link: "https://www.instagram.com/varandeck?igsh=MzNtMGdpZTVydTU2"
      },
      {
         titulo : "Nakka Bar",
-        descricao : "Endereço:",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        descricao : "Endereço:Rua Vilma, 51 - São Miguel Paulista, São Paulo - SP, 08060-090",
+        link: "https://www.instagram.com/nakkabar?igsh=MWNwY2N2dzVmb2Jjdg=="
      },
      {
         titulo : "itaquera beach bar",
-        descricao : "Endereço:",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+        descricao : "Endereço:Rua São Teodoro, 193 - Itaquera, São Paulo - SP, 08290-005",
+        link: "https://www.instagram.com/itaquerabeachbar?igsh=MWx3bHRwemxkZjB2Nw=="
      },
      {
         titulo : "Amém lounge Store",
-        descricao : "Endereço:",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
-     }
+        descricao : "Endereço:Rua Francisco Alarico Bérgamo, 80 - Vila Taquari, São Paulo - SP, 08230-010",
+        link: "https://www.instagram.com/amemloungestore?igsh=MWFseGxyNHU3Zzhlaw=="
+     },
+     {
+      titulo : "Nakka Club",
+      descricao : "Endereço:Av. Amador Bueno da Veiga, 751 - Vila Lais, São Paulo - SP, 03635-000",
+      link: "https://www.instagram.com/nakkaclub/p/CzJkp9WrtzD"
+   },
+   {
+      titulo : "Bar Pitangueira",
+      descricao : "Endereço:R. Siqueira Campos, 252 - Centro de Guarulhos, Guarulhos - SP, 07110-110",
+      link: "https://www.instagram.com/barpitangueira"
+   }
 
  ];
