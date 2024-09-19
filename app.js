527348923afe28fab28c5007b348761138b45937
Fernando Sousa Matos <est-fernando.matos@tjsp.jus.br>
2024-09-19 12:48:08 -0300

    filters

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ for (let dado of dados) {
                 <img src="${dado.imagem}" alt= "imagem do local" class="imagem">
                 <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
                 <p class="descricao-meta">${dado.descricao}</p>
+                <p class="descricao-meta">${dado.local}</p>
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>`
     }
@@ -35,4 +36,28 @@ for (let dado of dados) {
 section.innerHTML = resultados
 
 
+}
+// funcionalidade das tags
+
+const resultados = [
+    { id: 1, nome: 'varandeck', tags: ['tag1', 'tag2'] },
+    { id: 2, nome: 'nakka', tags: ['tag2', 'tag3'] },
+    { id: 3, nome: 'Resultado 3', tags: ['tag1', 'tag3'] },
+    // Adicione mais resultados conforme necessário
+];
+
+function filtrarResultados() {
+    const inputTags = document.getElementById('tags').value.split(',').map(tag => tag.trim());
+    const resultadosFiltrados = resultados.filter(resultado => 
+        resultado.tags.some(tag => inputTags.includes(tag))
+    );
+
+    const divResultados = document.getElementById('resultados');
+    divResultados.innerHTML = '';
+
+    resultadosFiltrados.forEach(resultado => {
+        const div = document.createElement('div');
+        div.textContent = `${resultado.nome} (Tags: ${resultado.tags.join(', ')})`;
+        divResultados.appendChild(div);
+    });
 }
--- dados.js
+++ dados.js
@@ -1,45 +1,51 @@
 let dados= [
     {
         titulo : "Varandeck",
-        descricao : "local com ótimos valores, tem a area externa e interna, com boa variedade de bebidas e costuma lotar aos finais de semana. Nota 4.6 no google",
+        descricao : "Local com ótimos valores, tem a area externa e interna, com boa variedade de bebidas e costuma lotar aos finais de semana. Nota 4.6 no google",
         local:"Endereço:Rua Irineu Bonardi, 173 - Vila Pedroso, São Paulo - SP, 08011-180",
         link: "https://www.instagram.com/varandeck/",
-        imagem:"imagens/ftvarandeck.png"
+        imagem:"imagens/ftvarandeck.png",
+        tags: ['Lounge Bar']
      },
      {
         titulo : "Nakka Bar",
-        descricao : "local com boas bebidas, atendentes com bom humor e muito frequentado por quem mora em são miguel. Nota 4.4 no google",
+        descricao : "Local com boas bebidas, atendentes com bom humor e muito frequentado por quem mora em são miguel. Nota 4.4 no google",
         local:"Endereço:Rua Vilma, 51 - São Miguel Paulista, São Paulo - SP, 08060-090",
         link: "https://www.instagram.com/nakkabar?igsh=MWNwY2N2dzVmb2Jjdg==",
-        imagem:"imagens/ftnakkabar.png"
+        imagem:"imagens/ftnakkabar.png",
+        tags: ['Lounge Bar']
      },
      {
-        titulo : "itaquera beach bar",
-        descricao : "Endereço:Rua São Teodoro, 193 - Itaquera, São Paulo - SP, 08290-005",
+        titulo : "Itaquera beach bar",
+        descricao : "Local bem arejado, tem a opção de poder usar as quadras para esportes com areia,",
         local:"Endereço:Rua São Teodoro, 193 - Itaquera, São Paulo - SP, 08290-005",
         link: "https://www.instagram.com/itaquerabeachbar?igsh=MWx3bHRwemxkZjB2Nw==",
-        imagem: ""
+        imagem: "",
+        tags: ['Lounge Bar']
      },
      {
         titulo : "Amém lounge Store",
-        descricao : "Endereço:Rua Francisco Alarico Bérgamo, 80 - Vila Taquari, São Paulo - SP, 08230-010",
+        descricao : "Local bem completo, djs muito bons, sempre lotado após as 23h",
         local:"Endereço:Rua Francisco Alarico Bérgamo, 80 - Vila Taquari, São Paulo - SP, 08230-010",
         link: "https://www.instagram.com/amemloungestore?igsh=MWFseGxyNHU3Zzhlaw==",
-        imagem:"imagens/ftamem.png"
+        imagem:"imagens/ftamem.png",
+        tags: ['Lounge Bar']
      },
      {
       titulo : "Nakka Club",
-      descricao : "Endereço:Av. Amador Bueno da Veiga, 751 - Vila Lais, São Paulo - SP, 03635-000",
+      descricao : "Local costuma ter djs diferentes tocando e além de ter variedade de bebidas tem um local fechado que é ideal em dias frios, dentro do local antes de entrar no salão princial a varias produtos a venda para fumantes",
       local:"Endereço:Av. Amador Bueno da Veiga, 751 - Vila Lais, São Paulo - SP, 03635-000",
       link: "https://www.instagram.com/nakkaclub/p/CzJkp9WrtzD",
-      imagem: "imagens/ftnakkaclub.png"
+      imagem: "imagens/ftnakkaclub.png",
+      tags: ['Lounge Bar']
    },
    {
       titulo : "Bar Pitangueira",
-      descricao : "Endereço:R. Siqueira Campos, 252 - Centro de Guarulhos, Guarulhos - SP, 07110-110",
+      descricao : "Local aberto com tendas para cobrir algumas partes e cadeiras de praia, uma parte interna e eventos no local ",
       local:"Endereço:R. Siqueira Campos, 252 - Centro de Guarulhos, Guarulhos - SP, 07110-110",
       link: "https://www.instagram.com/barpitangueira",
-      imagem: ""
+      imagem: "imagens/ftbarpitangueira.jpg",
+      tags: ['Lounge Bar']
    }
 
  ];
