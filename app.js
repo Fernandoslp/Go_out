154d7128b50479354cb0251097621584bb11c0c5
Fernando Sousa Matos <est-fernando.matos@tjsp.jus.br>
2024-09-30 12:20:38 -0300

    unificando resultados dos dois tipos de pesquisa

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ function pesquisar() {
     let section = document.getElementById("resultados-pesquisa")
   
     
-    
-
 let campoPesquisa = document.getElementById("campo-pesquisa").value
 if(campoPesquisa ==""){
     section.innerHTML = "<p>Nada foi encontrado</p>"
@@ -44,7 +42,7 @@ section.innerHTML = resultados
 
 // função das tags
 const tags = document.querySelectorAll('.tags input[type="checkbox"]');
-const resultados = document.getElementById('resultados');
+const resultados = document.getElementById('resultados-pesquisa');
 
 
 
@@ -60,8 +58,16 @@ function filtrarDados() {
     resultados.innerHTML = '';
     resultadosFiltrados.forEach(dado => {
         const div = document.createElement('div');
-        div.classList.add('resultado');
-        div.textContent = dado.titulo;
+        div.classList.add('resultados-pesquisa');
+        div.innerHTML = `
+        <div class="item-resultado">
+            <img src="${dado.imagem}" alt= "imagem do local" class="imagem">
+            <h2 class="titulo">${dado.titulo}</h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <p class="descricao-meta">${dado.local || 'Autor não informado'}</p>
+            <a href=${dado.link} target="_blank">Mais informações</a>
+            <p>${dado.tags}</p>
+        </div>`;
         resultados.appendChild(div);
     });
 }
