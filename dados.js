c406a68ee0cf8486299e5e9c0608219fd4d26f67
Fernando Sousa Matos <est-fernando.matos@tjsp.jus.br>
2024-10-14 12:51:23 -0300

    att dados e tags

--- dados.js
+++ dados.js
@@ -5,7 +5,7 @@ let dados= [
         local:"Endereço:Rua Irineu Bonardi, 173 - Vila Pedroso, São Paulo - SP, 08011-180",
         link: "https://www.instagram.com/varandeck/",
         imagem:"imagens/ftvarandeck.png",
-        tags: ['Lounge Bar', 'área externa', 'Entrada paga'],
+        tags: ['Lounge Bar', 'área externa', 'Entrada paga','mais baratos'],
         id: 1
      },
      {
@@ -23,7 +23,7 @@ let dados= [
         local:"Endereço:Rua São Teodoro, 193 - Itaquera, São Paulo - SP, 08290-005",
         link: "https://www.instagram.com/itaquerabeachbar?igsh=MWx3bHRwemxkZjB2Nw==",
         imagem: "imagens/ftitaquerabeachbar.jpg",
-        tags: ['Lounge Bar', 'área externa'],
+        tags: ['Lounge Bar', 'área externa','mais baratos'],
         id: 3
      },
      {
@@ -50,7 +50,7 @@ let dados= [
       local:"Endereço:R. Siqueira Campos, 252 - Centro de Guarulhos, Guarulhos - SP, 07110-110",
       link: "https://www.instagram.com/barpitangueira",
       imagem: "imagens/ftbarpitangueira.jpg",
-      tags: ['Lounge Bar','Entrada paga'],
+      tags: ['Lounge Bar','Entrada paga','área externa'],
       id: 6
    },
    {
@@ -76,9 +76,37 @@ let dados= [
       descricao : "Local com parte interna e algumas mesas na calçada, drinks variados e projetor que passa o jogos de futebol geralmente",
       local:"Endereço:R. Tiago Ferreira, 390 - Vila Pedroso, São Paulo - SP, 08011-270",
       link: "https://www.instagram.com/drinkeria.lux",
-      imagem: "imagens/ftbardojuizimperador.jpg",
+      imagem: "imagens/ftdrinkerialux.jpg",
       tags: ['Lounge Bar','Entrada gratis'],
       id: 9
+   },
+   {
+      titulo : "The Place Drinks",
+      descricao : `Local com parte interna e externa com bastante espaço, drinks variados e sempre lotado 
+      com eventos variados que pode podem ser conferidos pelas redes socias do local`,
+      local:"Endereço:Av. São Miguel 5057 (Parque Boturussu), São Paulo, SP ",
+      link: "https://www.instagram.com/theplacedrinks?igsh=aHU3bmx6c2ZpMW0w",
+      imagem: "imagens/fttheplacedrinks.jpg",
+      tags: ['Lounge Bar','Entrada paga'],
+      id: 10
+   },
+   {
+      titulo : "Prainha Lounge Bar",
+      descricao : "Local com parte interna com bastante espaço e um leve canto externo, drinks variados e ótimos preços",
+      local:"Endereço:Av. São Miguel 5057 (Parque Boturussu), São Paulo, SP ",
+      link: "https://www.instagram.com/prainhaloungebar?igsh=djNsOHhhYTVuMWtn",
+      imagem: "imagens/ftprainhaloungebar.jpg",
+      tags: ['Lounge Bar','Entrada paga','mais baratos'],
+      id: 11
+   },
+   {
+      titulo : "Dos Crias Bar",
+      descricao : "Local agradável com bons preços parte interna mais algumas mesas na calçada do local",
+      local:"Endereço:R. Vilma, 145 - Vila Jacuí, São Paulo - SP, 08060-090 ",
+      link: "https://www.instagram.com/doscriasbar?igsh=YzduMXB2NWpyc2Y1",
+      imagem: "imagens/ftdoscriasbar.jpg",
+      tags: ['Lounge Bar','Entrada gratis','mais baratos'],
+      id: 11
    },
 
  ];
